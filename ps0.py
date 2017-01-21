6fdf6b91ee2450a880a9064b55ad895083e778b5
Peter Kim <p0179417@gmail.com>
2017-01-21 14:24:56 -0500

    First commit, for the first assignment.

---
+++ ps0.py
@@ -0,0 +1,12 @@
+#Problem Set 0
+#Name: Peter Kim
+#Collaborators: ???
+#Time: 1:00
+#
+
+
+lname = raw_input('Enter your last name:')
+fname = raw_input('Enter your first name:')
+print lname
+print fname
+
